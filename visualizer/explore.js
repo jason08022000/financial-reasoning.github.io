80fa0b63d84cc7901673546d37fa02030cc09ce7
Jiachen Xu <jiachen023212@gmail.com>
2024-12-11 16:31:07 +1300

    Thirtieth update test website with table display capability

--- visualizer/explore.js
+++ visualizer/explore.js
@@ -2,7 +2,7 @@
 DATA_FILE = "data_public.js"; // default, answers for development, no answer for test
 
 // Variables for the filters with the number of questions
-let number_options = [1, 20, 50, 100, 150, 200];  
+let number_options = [1, 20, 50, 100, 150, 200,10000];  
 let splits = ["All", "train (2560)", "test (640)"];    
 let topic = [
     "All",
