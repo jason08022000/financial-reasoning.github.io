9baa5ef77406264e02bd34d11682c47de73e110a
Jiachen Xu <jiachen023212@gmail.com>
2025-01-29 17:27:18 +1300

    V1 FMP update cont

--- visualizer/explore.js
+++ visualizer/explore.js
@@ -20,8 +20,7 @@ let topic = [
     "Derivatives",
     "Market Risk",
     "Corporate Finance",
-    "Liquidity and Treasury Risk",
-    "Financial Markets and Productsd"
+    "Liquidity and Treasury Risk"
 ]
 
 
